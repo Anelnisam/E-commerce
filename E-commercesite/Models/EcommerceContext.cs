67e89b81e2d824337b92d2d9271f4d2c946056e9
Anel <anel_taric96@live.com>
2019-04-08 18:41:18 +0200

    idemo

--- E-commercesite/Models/EcommerceContext.cs
+++ E-commercesite/Models/EcommerceContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_commercesite.Models;
 
 
 namespace E_commercesite.Models
@@ -17,5 +18,6 @@ namespace E_commercesite.Models
         {
 
         }
+        public DbSet<E_commercesite.Models.Food> Food { get; set; }
     }
 }
---
+++ E-commercesite/Models/Food.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commercesite.Models
+{
+    public class Food
+    {
+        [Key]
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public float Price { get; set; }
+
+
+    }
+}
